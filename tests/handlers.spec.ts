8db05f873e13dccd826aea43893c839ec2a990ac
stefanodav <stefano.davella@gmail.com>
2023-05-19 22:21:38 +0200

    fix test

--- tests/handlers.spec.ts
+++ tests/handlers.spec.ts
@@ -1,6 +1,6 @@
 import { test } from "@japa/runner";
 import { parseAndHandleRequest } from "../src/parseAndHandleRequest.js";
-import { model } from "../src/handler.js";
+import { model } from "../src/bot.js";
 
 test.group("ciccio", () => {
     test("culo", async ({ expect }) => {

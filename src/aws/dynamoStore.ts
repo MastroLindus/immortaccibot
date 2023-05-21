78cf84044af4df1dc37c0e1c6ad29262bf567541
stefanodav <stefano.davella@gmail.com>
2023-05-21 09:43:34 +0200

    add log for dynamo

--- src/aws/dynamoStore.ts
+++ src/aws/dynamoStore.ts
@@ -4,9 +4,12 @@ import { options } from "../model.js";
 const client = new DynamoDBClient({ region: options.aws.region });
 
 export async function listTables() {
+    console.log("INVOKING LIST TABLES");
     const command = new ListTablesCommand({});
     try {
         const results = await client.send(command);
+        console.log("RESULTS");
+        console.log(results);
         return results.TableNames!.join("\n");
     } catch (err) {
         console.error(err);

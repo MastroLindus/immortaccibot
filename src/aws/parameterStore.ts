52840a2abb72c37f0f63001572194983ec3d243d
stefanodav <stefano.davella@gmail.com>
2023-05-21 15:01:08 +0200

    clean up aws stuff

--- src/aws/parameterStore.ts
+++ src/aws/parameterStore.ts
@@ -5,21 +5,24 @@ const client = new SSMClient({ region: options.aws.region });
 
 export async function getAwsParametersFromStore<T extends string>(
     parameterNames: ReadonlyArray<T>
-): Promise<Record<T, string>> {
+): Promise<Record<T, string> | undefined> {
     const params = {
         Names: [...parameterNames],
         WithDecryption: true,
     };
     const command = new GetParametersCommand(params);
-    const response = await client.send(command);
-
-    return (
-        response?.Parameters?.reduce<Record<string, string>>(
-            (result: Record<string, string>, k: Parameter) => ({
-                ...result,
-                [k.Name!]: k.Value ?? "",
-            }),
-            {}
-        ) ?? {}
-    );
+    try {
+        const response = await client.send(command);
+        return (
+            response?.Parameters?.reduce<Record<string, string>>(
+                (result: Record<string, string>, k: Parameter) => ({
+                    ...result,
+                    [k.Name!]: k.Value ?? "",
+                }),
+                {}
+            ) ?? {}
+        );
+    } catch (err) {
+        console.log(err);
+    }
 }
